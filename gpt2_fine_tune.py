0dc13e54fffdca710866a006cf834517208ab2c5
Aanal Mahesh Sonara <62333645+Aanal2901@users.noreply.github.com>
2021-05-08 15:49:17 +0530

    Update gpt2_fine_tune.py

--- gpt2_fine_tune.py
+++ gpt2_fine_tune.py
@@ -2,12 +2,14 @@
 
 ### If you run for 10 epochs (training takes longer) you use all the checkpoints, otherwise you will have to use till checkpoint < step
 ## this is buiding model from scratch
+
 from transformers import GPT2Config, GPT2LMHeadModel, GPT2TokenizerFast
 import torch
 
 config = GPT2Config()
 
 tokenizer = GPT2TokenizerFast.from_pretrained('gpt2', max_len=512)
+tokenizer.add_tokens('<pad>')
 tokenizer.pad_token = '<pad>'
 
 model = GPT2LMHeadModel(config=config)
@@ -25,13 +27,15 @@ from transformers import DataCollatorForLanguageModeling
 data_collator = DataCollatorForLanguageModeling(
     tokenizer=tokenizer, mlm=False
 )
+model.resize_token_embeddings(len(tokenizer))
+
 
 from transformers import Trainer, TrainingArguments
 
 training_args = TrainingArguments(
     output_dir="./GPT2new-2",
     overwrite_output_dir=True,
-    num_train_epochs=4,
+    num_train_epochs=1,
     per_gpu_train_batch_size=32,
     save_steps=10_000,
     save_total_limit=2,
@@ -57,9 +61,9 @@ import torch
 
 config = GPT2Config()
 
-tokenizer = GPT2TokenizerFast.from_pretrained('gpt2')
+tokenizer = GPT2TokenizerFast.from_pretrained('gpt2', max_len = 512)
 # tokenizer.add_tokens('<pad>')
-# tokenizer.pad_token = '<pad>'
+tokenizer.pad_token = '<pad>'
 
 model = GPT2LMHeadModel.from_pretrained('gpt2', config=config)
 
@@ -67,11 +71,11 @@ from transformers import LineByLineTextDataset
 
 dataset = LineByLineTextDataset(
     tokenizer=tokenizer,
-    file_path="all.txt",
+    file_path="../input/romcom/all.txt",
     block_size=128,
 )
 
-#tokenizer.add_tokens('<pad>')
+tokenizer.add_tokens('<pad>')
 tokenizer.pad_token = '<pad>'
 
 from transformers import DataCollatorForLanguageModeling
@@ -80,12 +84,14 @@ data_collator = DataCollatorForLanguageModeling(
     tokenizer=tokenizer, mlm=False
 )
 
+model.resize_token_embeddings(len(tokenizer))
+
 from transformers import Trainer, TrainingArguments
 
 training_args = TrainingArguments(
     output_dir="./GPT2final",
     overwrite_output_dir=True,
-    num_train_epochs=10,
+    num_train_epochs=1,
     per_gpu_train_batch_size=32,
     save_steps=5_000,
     save_total_limit=25,
@@ -97,6 +103,7 @@ trainer = Trainer(
     args=training_args,
     data_collator=data_collator,
     train_dataset=dataset,
+    
 )
 
 trainer.train()
