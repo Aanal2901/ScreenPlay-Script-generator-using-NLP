2b9c5180aaa3cf715bbb6cf26b425186bff62634
Aanal Mahesh Sonara <62333645+Aanal2901@users.noreply.github.com>
2021-05-08 17:08:12 +0530

    Update gpt2_fine_tune.py

--- gpt2_fine_tune.py
+++ gpt2_fine_tune.py
@@ -31,7 +31,6 @@ model.resize_token_embeddings(len(tokenizer))
 
 
 from transformers import Trainer, TrainingArguments
-
 training_args = TrainingArguments(
     output_dir="./GPT2new-2",
     overwrite_output_dir=True,
@@ -91,7 +90,7 @@ from transformers import Trainer, TrainingArguments
 training_args = TrainingArguments(
     output_dir="./GPT2final",
     overwrite_output_dir=True,
-    num_train_epochs=1,
+    num_train_epochs=4,
     per_gpu_train_batch_size=32,
     save_steps=5_000,
     save_total_limit=25,
@@ -110,32 +109,34 @@ trainer.train()
 
 trainer.save_model("./GPT2final")
 
-"""# Checking points"""
-
-checkpoints = ['checkpoint-5000', 'checkpoint-10000', 'checkpoint-15000', 'checkpoint-20000', 'checkpoint-25000', 'checkpoint-30000', 'checkpoint-35000', 'checkpoint-40000', 'checkpoint-45000', 
- 'checkpoint-50000', 'checkpoint-55000', 'checkpoint-60000', 'checkpoint-65000', 'checkpoint-70000', 'checkpoint-75000', 'checkpoint-80000']
+### For using Checkpoints
+import numpy as np
+checkpoints = ['checkpoint-5000']
 checkdict = {}
 
 for i in checkpoints:
   checkdict[i] = GPT2LMHeadModel.from_pretrained('./GPT2final/' + i, config=config)
+  checkdict[i] = checkdict[i].to(device)
 checkdict['gpt'] = model
+checkdict['gpt'] = checkdict['gpt'].to(device)
 checkdict['final'] = GPT2LMHeadModel.from_pretrained('./GPT2final/', config=config)
-
+checkdict['final'] = checkdict['final'].to(device)
 checkdict['new'] = GPT2LMHeadModel.from_pretrained('./GPT2new-2/', config=config)
-
-checkdict['new-1'] = GPT2LMHeadModel.from_pretrained('./GPT2new-2/checkpoint-90000/', config=config)
+checkdict['new'] = checkdict['new'].to(device)
+print(1)
 
 love_input = torch.tensor(tokenizer.encode("love", add_special_tokens=True)).unsqueeze(0)
+love_input = love_input.to(device)
 m = torch.nn.Softmax(dim=2)
 
 plove = {}
 for i in checkpoints:
   plove[i] = torch.distributions.categorical.Categorical(m(checkdict[i](love_input)[0]))
 
-for i in ['gpt', 'final', 'new', 'new-1']:
+for i in ['gpt', 'final', 'new']:
   plove[i] = torch.distributions.categorical.Categorical(m(checkdict[i](love_input)[0]))
 
-KLlovenew = torch.distributions.kl.kl_divergence(plove['gpt'], plove['new'])[0][0].detach().numpy()
+KLlovenew = torch.distributions.kl.kl_divergence(plove['gpt'], plove['new'])[0][0].cpu().detach().numpy()
 print(KLlovenew)
 
 KLlove = np.zeros(len(checkpoints))
@@ -144,67 +145,78 @@ ind = 0
 for i in checkpoints:
   # print(i + ':')
   KL = torch.distributions.kl.kl_divergence(plove['gpt'], plove[i])[0][0]
-  KLlove[ind] = KL.detach().numpy()
+  KLlove[ind] = KL.cpu().detach().numpy()
   ind += 1
+  print(ind)
 
 pthe = {}
 KLthe = np.zeros(len(checkpoints))
 ind = 0
 the_input = torch.tensor(tokenizer.encode("the", add_special_tokens=True)).unsqueeze(0)
+the_input = the_input.to(device)
+
 for i in checkpoints:
+    
   pthe[i] = torch.distributions.categorical.Categorical(m(checkdict[i](the_input)[0]))
-for i in ['gpt', 'final', 'new', 'new-1']:
+for i in ['gpt', 'final', 'new']:
   pthe[i] = torch.distributions.categorical.Categorical(m(checkdict[i](the_input)[0]))
 for i in checkpoints:
   # print(i + ':')
   KL = torch.distributions.kl.kl_divergence(pthe['gpt'], pthe[i])[0][0]
-  KLthe[ind] = KL.detach().numpy()
+  KLthe[ind] = KL.cpu().detach().numpy()
   ind += 1
 
-KLthenew = torch.distributions.kl.kl_divergence(pthe['gpt'], pthe['new'])[0][0].detach().numpy()
+KLthenew = torch.distributions.kl.kl_divergence(pthe['gpt'], pthe['new'])[0][0].cpu().detach().numpy()
 print(KLthenew)
 
 pdereg = {}
 KLdereg = np.zeros(len(checkpoints))
 ind = 0
 dereg_input = torch.tensor(tokenizer.encode(" deregulation", add_special_tokens=True)).unsqueeze(0)
+dereg_input = dereg_input.to(device)
+
 for i in checkpoints:
+    
   pdereg[i] = torch.distributions.categorical.Categorical(m(checkdict[i](dereg_input)[0]))
-for i in ['gpt', 'final', 'new', 'new-1']:
+for i in ['gpt', 'final', 'new']:
   pdereg[i] = torch.distributions.categorical.Categorical(m(checkdict[i](dereg_input)[0]))
+print(3)
 for i in checkpoints:
   print(i + ':')
   KL = torch.distributions.kl.kl_divergence(pdereg['gpt'], pdereg[i])[0][0]
   print(KL)
-  KLdereg[ind] = KL.detach().numpy()
+  KLdereg[ind] = KL.cpu().detach().numpy()
   ind += 1
 
-KLderegnew = torch.distributions.kl.kl_divergence(pdereg['gpt'], pdereg['new'])[0][0].detach().numpy()
+KLderegnew = torch.distributions.kl.kl_divergence(pdereg['gpt'], pdereg['new'])[0][0].cpu().detach().numpy()
 print(KLderegnew)
 
 pexc = {}
 KLexc = np.zeros(len(checkpoints))
 ind = 0
 exc_input = torch.tensor(tokenizer.encode("!", add_special_tokens=True)).unsqueeze(0)
+exc_input = exc_input.to(device)
 for i in checkpoints:
   pexc[i] = torch.distributions.categorical.Categorical(m(checkdict[i](exc_input)[0]))
-for i in ['gpt', 'final', 'new', 'new-1']:
+for i in ['gpt', 'final', 'new']:
   pexc[i] = torch.distributions.categorical.Categorical(m(checkdict[i](exc_input)[0]))
 for i in checkpoints:
   print(i + ':')
   KL = torch.distributions.kl.kl_divergence(pexc['gpt'], pexc[i])[0][0]
   print(KL)
-  KLexc[ind] = KL.detach().numpy()
+  KLexc[ind] = KL.cpu().detach().numpy()
   ind += 1
 
-KLexcnew = torch.distributions.kl.kl_divergence(pexc['gpt'], pexc['new'])[0][0].detach().numpy()
+KLexcnew = torch.distributions.kl.kl_divergence(pexc['gpt'], pexc['new'])[0][0].cpu().detach().numpy()
 print(KLexcnew)
 
 import numpy as np
 
 import matplotlib.pyplot as plt
 
-checkno = np.array([500, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000, 5500, 6000, 6500, 7000, 7500, 8000]) * 10
+
+checkno = np.array([checkno = np.array([500, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000, 5500, 6000, 6500, 7000, 7500, 8000]) * 10])
+
 loss = np.array([1.4465, 1.3898, 1.3757, 1.3303, 1.3323, 1.2391, 1.2671, 1.2348, 1.2786, 1.2569, 1.1911, 1.2154, 1.1853, 1.2104, 1.1965, 
                  1.1342, 1.1596, 1.1586, 1.1675, 1.1498, 1.1050, 1.0867, 1.1283, 1.1257, 1.1336, 1.1354, 1.0625, 1.0395, 1.0753, 1.0788, 
                  1.0821, 1.0371, 1.0417, 1.0475, 1.0464, 1.0827, 1.0405, 1.0213, 1.0125, 1.0274]
@@ -230,39 +242,15 @@ plt.plot(lossno, loss)
 plt.xlabel('Iteration')
 plt.ylabel('Loss')
 
-## prompts to check output
-prompt = '''JOHNNY
-Well because it was an out of state bank. Anyway, I was working as a busboy in a hotel, and she was sitting, drinking her coffee, and she was so beautiful, and I say hi to her. That’s how we met.
-MARK
-So, I mean, what's the interesting part?
-JOHNNY
-Well the interesting part is that
-'''
+## Prompt:
+prompt = 
 inputs = tokenizer.encode(prompt, add_special_tokens=True, return_tensors="pt")
-outputs = checkdict['gpt'].generate(inputs, max_length=300, do_sample=True, top_p=0.95, top_k=100, temperature=1.1)
+inputs = inputs.to(device)
 
-tokenizer.decode(outputs[0].numpy())
+## for model made from scratch
+outputs = checkdict['new'].generate(inputs, max_length=300, do_sample=True, top_p=0.95, top_k=100, temperature=1.1)
+tokenizer.decode(outputs[0].cpu().numpy())
 
-outputs = checkdict['new'].generate(inputs, max_length=300, do_sample=True, top_p=0.95, top_k=100, temperature=0.8)
-
-tokenizer.decode(outputs[0].numpy())
-
-prompt2 = '''ILSA
-But what about us?
-RICK
-We'll always have Paris. We didn't have, we, we lost it until you came to Casablanca. We got it back last night
-ILSA
-When I said I would never leave you'''
-inputs2 = tokenizer.encode(prompt2, add_special_tokens=True, return_tensors="pt")
-outputs2 = checkdict['gpt'].generate(inputs2, max_length=300, do_sample=True, top_p=0.95, top_k=100, temperature=1.1)
-
-tokenizer.decode(outputs2[0].numpy())
-
-outputs2a = checkdict['checkpoint-80000'].generate(inputs2, max_length=300, do_sample=True, top_p=0.95, top_k=100, temperature=1.1)
-
-tokenizer.decode(outputs2a[0].numpy())
-
-outputs2a = checkdict['checkpoint-10000'].generate(inputs2, max_length=300, do_sample=True, top_p=0.95, top_k=100, temperature=1.1)
-
-tokenizer.decode(outputs2a[0].numpy())
-
+## for fine tuned model
+outputs = checkdict['final'].generate(inputs, max_length=300, do_sample=True, top_p=0.95, top_k=100, temperature=1.1)
+tokenizer.decode(outputs[0].cpu().numpy())
